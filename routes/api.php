49c6b2759a275e897f0b0015c7d5130a70f9c293
AlessioMartella7 <alessio.mart87@gmail.com>
2024-11-20 17:21:07 +0100

    added api ProjectController

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\ProjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::controller(ProjectController::class)->name('api.')->group(function(){
+    Route::get('projects', 'index')->name('projects.index');
 });
